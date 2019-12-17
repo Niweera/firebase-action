5d4bfc7b80c68d2c082fba155f932c423839f3d5
Niweera <w.nipuna@gmail.com>
2019-12-17 13:02:09 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-echo "\npwd\n" 
-pwd
-
-echo "\nls\n"
-ls
-
 if [ -z "$FIREBASE_TOKEN" ]; then
     echo "FIREBASE_TOKEN is required to run commands with the firebase cli"
     exit 126
@@ -21,14 +15,6 @@ if [ -n "$PROJECT_ID" ]; then
     echo "setting firebase project to $PROJECT_ID"
     firebase use --add $PROJECT_ID
 fi
-
-echo "\nproject path is:" $PROJECT_PATH "\n"
-
-echo "\npwd\n" 
-pwd
-
-echo "\nls\n"
-ls
 
 echo "\nnow running firebase actions\n"
 sh -c "firebase $*"
